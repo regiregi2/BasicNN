d0f410056981b0aace8fdae90b35cf67698f9cfe
Regi <revaineikis@gmail.com>
2020-10-03 18:17:08 +0100

    Added basic structure of network class

---
+++ BasicNN.py
@@ -0,0 +1,16 @@
+class BasicNN:
+
+    # TODO add extra params that with default values that can be tweaked to optimise network
+    def __init__(self, input_data, output_data):
+        self.input_data = input_data
+        self.output_data = output_data
+        pass
+
+    def train(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
